45c820a3e5f92fd4e12229f627778118ff130aac
noth-mac <759213373@qq.com>
2026-04-23 13:12:57 +0800

    初始化配置文件软链接

---
+++ CLI/fish/config.fish
@@ -0,0 +1,61 @@
+test ! -e "$HOME/.x-cmd.root/local/data/fish/rc.fish" || source "$HOME/.x-cmd.root/local/data/fish/rc.fish" # boot up x-cmd.
+if status is-interactive
+# Commands to run in interactive sessions can go here
+	# ── Homebrew Shell Environment
+		if test -x /opt/homebrew/bin/brew
+		/opt/homebrew/bin/brew shellenv | source
+	end
+
+	# Starship
+		if command -q starship
+			starship init fish | source
+		end
+
+	# ── FZF
+		set -gx FZF_DEFAULT_OPTS "\
+			--height 75% \
+			--layout=reverse \
+			--border \
+			--info=inline"	
+		set -gx FZF_DEFAULT_COMMAND 'fd --hidden --follow --exclude .git'
+		set -g fzf_fd_opts --hidden --follow --exclude .git
+		set -g fzf_preview_dir_cmd eza --all --color=always --icons --git --tree --level=2
+		set -g fzf_preview_file_cmd bat --style=numbers --color=always --line-range :500
+		set -g fzf_history_time_format %d-%m-%y
+
+		fzf_configure_bindings --directory=\cf --history=\cr
+	
+	# zoxide cd改成z
+    if type -q zoxide
+        zoxide init fish | source
+		abbr -a cd z
+    end
+end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file=$tmp
+    set cwd (cat $tmp)
+    if test "$cwd" != "$PWD" -a -d "$cwd"
+        builtin cd -- $cwd
+    end
+    rm -f $tmp
+end
+
+#bat主题
+set -gx BAT_THEME "Catppuccin Mocha"
+
+# 别名
+	# Eza
+    abbr -a el 'eza --long --header --icons --git --all'
+    abbr -a et 'eza --tree --level=2 --long --header --icons --git'
+
+	# Homebrew
+    abbr -a bi 'brew install'
+    abbr -a bui 'brew uninstall --zap'
+    abbr -a bs 'brew search'
+    abbr -a bif 'brew info'
+    abbr -a bl 'brew leaves; and brew list --cask'
+    abbr -a bd 'brew deps --installed --tree'
+    abbr -a bu 'brew update; and brew upgrade'
+    abbr -a bc 'brew autoremove; and brew cleanup --prune=all'
